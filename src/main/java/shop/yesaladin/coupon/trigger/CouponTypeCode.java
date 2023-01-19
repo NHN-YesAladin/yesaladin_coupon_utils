6f2eb717ad24c84a02e78c3f2fb119b79b8ac40b
Hongdae Kim <mongmeo.dev@gmail.com>
2023-01-19 23:11:23 +0900

    CouponTypeCode 추가 및 displayName 추가

---
+++ src/main/java/shop/yesaladin/coupon/trigger/CouponTypeCode.java
@@ -0,0 +1,21 @@
+package shop.yesaladin.coupon.trigger;
+
+public enum CouponTypeCode {
+
+    FIXED_PRICE(1, "정액쿠폰"), FIXED_RATE(2, "정률쿠폰"), POINT(3, "포인트 충전 쿠폰");  // 쿠폰 종류 코드
+    private final int code;
+    private final String displayName;
+
+    CouponTypeCode(int code, String displayName) {
+        this.code = code;
+        this.displayName = displayName;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
--- src/main/java/shop/yesaladin/coupon/trigger/TriggerTypeCode.java
+++ src/main/java/shop/yesaladin/coupon/trigger/TriggerTypeCode.java
@@ -2,22 +2,28 @@ package shop.yesaladin.coupon.trigger;
 
 public enum TriggerTypeCode {
 
-    SIGN_UP(1),
-    BIRTHDAY(2),
-    COUPON_OF_THE_MONTH(3),
-    MEMBER_GRADE_WHITE(101),
-    MEMBER_GRADE_BRONZE(102),
-    MEMBER_GRADE_SILVER(103),
-    MEMBER_GRADE_GOLD(104),
-    MEMBER_GRADE_PLATINUM(105);
+    SIGN_UP(1, "회원가입 쿠폰"),
+    BIRTHDAY(2, "생일자 쿠폰"),
+    COUPON_OF_THE_MONTH(3, "이달의 쿠폰"),
+    MEMBER_GRADE_WHITE(101, "등급 쿠폰(화이트)"),
+    MEMBER_GRADE_BRONZE(102, "등급 쿠폰(브론즈)"),
+    MEMBER_GRADE_SILVER(103, "등급 쿠폰(실버)"),
+    MEMBER_GRADE_GOLD(104, "등급 쿠폰(골드)"),
+    MEMBER_GRADE_PLATINUM(105, "등급 쿠폰(플래티넘)");
 
     private final int code;
+    private final String displayName;
 
-    TriggerTypeCode(int code) {
+    TriggerTypeCode(int code, String displayName) {
         this.code = code;
+        this.displayName = displayName;
     }
 
     public int getCode() {
         return code;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 }
