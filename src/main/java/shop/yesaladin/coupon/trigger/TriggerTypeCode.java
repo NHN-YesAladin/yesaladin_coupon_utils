82f72b9cf41e87b26fc3b580f662f81b901aefc5
mj <squarearth1@gmail.com>
2023-02-01 00:46:55 +0900

    TriggerTypeCode 의 displayName 수정

--- src/main/java/shop/yesaladin/coupon/trigger/TriggerTypeCode.java
+++ src/main/java/shop/yesaladin/coupon/trigger/TriggerTypeCode.java
@@ -2,14 +2,14 @@ package shop.yesaladin.coupon.trigger;
 
 public enum TriggerTypeCode {
 
-    SIGN_UP(1, "회원가입 쿠폰"),
-    BIRTHDAY(2, "생일자 쿠폰"),
+    SIGN_UP(1, "회원가입"),
+    BIRTHDAY(2, "생일"),
     COUPON_OF_THE_MONTH(3, "이달의 쿠폰"),
-    MEMBER_GRADE_WHITE(101, "등급 쿠폰(화이트)"),
-    MEMBER_GRADE_BRONZE(102, "등급 쿠폰(브론즈)"),
-    MEMBER_GRADE_SILVER(103, "등급 쿠폰(실버)"),
-    MEMBER_GRADE_GOLD(104, "등급 쿠폰(골드)"),
-    MEMBER_GRADE_PLATINUM(105, "등급 쿠폰(플래티넘)");
+    MEMBER_GRADE_WHITE(101, "등급(화이트)"),
+    MEMBER_GRADE_BRONZE(102, "등급(브론즈)"),
+    MEMBER_GRADE_SILVER(103, "등급(실버)"),
+    MEMBER_GRADE_GOLD(104, "등급(골드)"),
+    MEMBER_GRADE_PLATINUM(105, "등급(플래티넘)");
 
     private final int code;
     private final String displayName;
